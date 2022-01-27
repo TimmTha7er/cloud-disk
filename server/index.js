ec22e3b7ccc2912aaec891fc10de883bce0474fa
Roman Petrashkevich <roman.petrashkevich@skdo.systems>
2022-01-27 19:45:29 +0300

    add server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log('h1llo node js')
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+const PORT = config.get('serverPort')
+const URL = config.get('dbUrl')
+
+const start = async () => {
+	try {
+		await mongoose.connect(URL)
+		
+		app.listen(PORT, () => {
+			console.log('Server started on port', PORT)
+		})
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
