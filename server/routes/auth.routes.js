c4833e907fdcefeb66ded4201083079641b19c6f
Roman Petrashkevich <roman.petrashkevich@skdo.systems>
2022-01-28 15:27:14 +0300

    Add jwt token authorization

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,4 +1,6 @@
+const config = require('config')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const Router = require('express')
 const User = require('../models/User')
 const {check, validationResult} = require('express-validator')
@@ -18,7 +20,6 @@ router.post('/registration',
 				return res.status(400).json({message: 'Uncorrect request', errors})
 			}
 
-
 			const {email, password} = req.body
 			const candidate = await User.findOne({email})
 
@@ -26,7 +27,7 @@ router.post('/registration',
 				return res.status(400).json({message: `User with email ${email} already exist`})
 			}
 
-			const hashPassword = await bcrypt.hash(password, 15)
+			const hashPassword = await bcrypt.hash(password, 7)
 			const user = new User({email, password: hashPassword})
 
 			await user.save()
@@ -36,6 +37,44 @@ router.post('/registration',
 			console.log(error)
 			res.send({message: 'Server error'})
 		}
-})
+	}
+)
+
+router.post('/login',
+	async (req, res) => {
+		try {
+			const {email, password} = req.body
+			const user = await User.findOne({email})
+
+			if (!user) {
+				return res.status(404).json({message: 'User not found'})
+			}
+
+
+			const isPassValid = bcrypt.compareSync(password, user.password)
+
+			if (!isPassValid) {
+				return res.status(404).json({message: 'Invalid password'})
+			}
+
+			const secretKey = config.get('secretKey')
+			const token = jwt.sign({id: user.id}, secretKey, {expiresIn: '1h'})
+
+			return res.json({
+				token,
+				user: {
+					id: user.id,
+					email: user.email,
+					diskSpace: user.diskSpace,
+					usedSpace: user.usedSpace,
+					avatar: user.avatar
+				}
+			})
+		} catch (error) {
+			console.log(error)
+			res.send({message: 'Server error'})
+		}
+	}
+)
 
 module.exports = router
